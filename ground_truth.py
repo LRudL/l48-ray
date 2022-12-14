92500045bc10716c424e0af7e19fcf05ced1a696
Anoushka <am2789@cam.ac.uk>
2022-12-14 17:00:13 +0530

    refactored naive implementation and plot_utils

--- ground_truth.py
+++ ground_truth.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 
+
 def get_Ht_test1(t):
     """A test function that implements a simple case of H_t"""
     E_0 = 1
@@ -12,23 +13,42 @@ def get_Ht_test1(t):
 
 
 def get_U_tplusdt(get_Ht, t, dt, U_t):
-    """Takes: a function for calculating H at a t, a t, a dt size,
-    and a value of U_t"""
+    """
+    This function calculates the unitary U after time (t+dt) given U at time t
+        @params:
+        get_Ht: a function for calculating the Hermitian matrix H at time t
+        t: the time until which we have simulated the value of U
+        dt: a small interval over which we want to understand how U changes
+        U_t: the unitary U at time t
+
+        @returns:
+        U(t+dt): the value of U after a time step
+    """
     I = np.eye(2, dtype = complex)
     i = complex(0, 1)
     return np.matmul(I - i*get_Ht(t + 0.5*dt)*dt, U_t)
 
 
 def naive_simulation(get_Ht, T, dt):
+    '''
+    This function returns the result of euler integration over time T
+        @params
+        get_Ht: the Hermitian function which changes U over time
+        T: the total duration over which to run the simulation
+        dt: the size of the timesteps
+        @returns
+        U(T): ground truth value of U at the end of the simulation
+        U_ts: value of U throughout the simulation
+    '''
     t = 0
     # U_0 is always I
-    U_t = np.eye(2, dtype = complex)
+    U_t = np.eye(2, dtype=complex)
 
-    Uts = []
-    
-    while (t<T):
+    U_ts = []
+
+    while (t < T):
         U_t = get_U_tplusdt(get_Ht, t, dt, U_t)
-        Uts.append(U_t)
         t += dt
-        
-    return Uts
+        U_ts.append(U_t)
+
+    return U_t, U_ts
--- plot_utils.py
+++ plot_utils.py
@@ -1,6 +1,70 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from qutip import Bloch
+from ground_truth import naive_simulation, get_Ht_test1
+
+
+def plot_U_elements(T, dt, get_Ht):
+    '''
+        Plots the four dimensions of the 2x2 matrix U over time T, calculating U at intervals sized dt
+    '''
+    x = np.linspace(0, T, int(T/dt)+1)
+    U_T, y = naive_simulation(get_Ht, T, dt)
+
+    y1 = [y[i][0,0] for i in range(len(y))]
+    y2 = [y[i][0,1] for i in range(len(y))]
+    y3 = [y[i][1,0] for i in range(len(y))]
+    y4 = [y[i][1,1] for i in range(len(y))]
+
+    fig = plt.figure(figsize=(10,20))
+    ax = plt.axes(projection ='3d')
+
+    y1r = [y1[i].real for i in range(len(y1))]
+    y1i = [y1[i].imag for i in range(len(y1))]
+    y2r = [y2[i].real for i in range(len(y2))]
+    y2i = [y2[i].imag for i in range(len(y2))]
+    y3r = [y3[i].real for i in range(len(y3))]
+    y3i = [y3[i].imag for i in range(len(y3))]
+    y4r = [y4[i].real for i in range(len(y4))]
+    y4i = [y4[i].imag for i in range(len(y4))]
+
+
+    ax.plot(x, y1r, y1i, label = "y1 (0,0)")
+    ax.plot(x, y2r, y2i, label = "y2 (0,1)")
+    ax.plot(x, y3r, y3i, label = "y3 (1,0)")
+    ax.plot(x, y4r, y4i, label = "y4 (1,1)")
+    ax.legend()
+    ax.set_xlabel("time t")
+    ax.set_ylabel("real")
+    ax.set_zlabel("complex")
+
+
+def plot_H_elements(get_Ht):
+    x = np.linspace(-5, 10, 300)
+    y = [get_Ht(t) for t in x]
+
+    y1 = [y[i][0, 0] for i in range(len(y))]
+    y2 = [y[i][0, 1] for i in range(len(y))]
+    y3 = [y[i][1, 0] for i in range(len(y))]
+    y4 = [y[i][1, 1] for i in range(len(y))]
+
+    fig = plt.figure(figsize=(10, 20))
+    ax = plt.axes(projection='3d')
+
+    y1r = [y1[i].real for i in range(len(y1))]
+    y1i = [y1[i].imag for i in range(len(y1))]
+    y2r = [y2[i].real for i in range(len(y2))]
+    y2i = [y2[i].imag for i in range(len(y2))]
+    y3r = [y3[i].real for i in range(len(y3))]
+    y3i = [y3[i].imag for i in range(len(y3))]
+    y4r = [y4[i].real for i in range(len(y4))]
+    y4i = [y4[i].imag for i in range(len(y4))]
+
+    ax.plot(x, y1r, y1i, label="y1 (0,0)")
+    ax.plot(x, y2r, y2i, label="y2 (0,1)")
+    ax.plot(x, y3r, y3i, label="y3 (1,0)")
+    ax.plot(x, y4r, y4i, label="y4 (1,1)")
+    ax.legend()
 
 
 def plot_bloch_sphere_evolution(dUs: np.ndarray, init_state: int = 0) -> None:
@@ -37,4 +101,10 @@ if __name__ == '__main__':
 
     plot_bloch_sphere_evolution(dUs)
 
+    plt.show()
+
+    plot_U_elements(50, 0.01, get_Ht_test1)
+    plt.show()
+
+    plot_H_elements(get_Ht_test1)
     plt.show()
