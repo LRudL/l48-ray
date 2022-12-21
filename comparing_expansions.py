087fdaec637043a17cc82f4656688323b93d8396
LRudL <rudolf.laine@gmail.com>
2022-12-21 22:29:58 +0200

    Change to small integrator_dt, same end value of t

--- comparing_expansions.py
+++ comparing_expansions.py
@@ -6,8 +6,8 @@ from utils import fidelity, unitary
 import numpy as np
 
 if __name__ == '__main__':
-    euler_ssq_Ut, euler_ssq_Uts = naive_simulation(hermitian_functions.single_spin_qubit, 5, 0.01)
+    euler_ssq_Ut, euler_ssq_Uts = naive_simulation(hermitian_functions.single_spin_qubit, 5, 0.0001)
     print("euler \n", euler_ssq_Ut)
     # magnus_ssq_Ut = magnus(magnus_expansion.get_At, 2, k=1)
-    magnus_ssq_Ut = magnus(hermitian_functions.single_spin_qubit, 2, k=1)
+    magnus_ssq_Ut = magnus(hermitian_functions.single_spin_qubit, 5, k=1, integrator_dt = 0.0001)
     print("magnus \n", magnus_ssq_Ut)
