d07d49bc7a07ef2540486387503833c1eeb78fc9
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2022-12-31 16:10:19 +0000

    Refactord stuff

--- hermitian_functions.py
+++ hermitian_functions.py
@@ -1,16 +1,17 @@
 import math
 import numpy as np
 
+
 # Class for systems of the form: H(t) = H_0 + v(t)V
 class ConstantMatrixHermitian:
     def __init__(self, H_0, get_vt, V):
         self.H_0 = H_0
         self.get_vt = get_vt
         self.V = V
-    
+
     def get_components(self):
         return (self.H_0, self.get_vt, self.V)
-    
+
     def at_t(self, t, *args):
         # NOTE: this throws away all other args
         # So passing in E_0, etc. in e.g. naive_simulation will not
@@ -23,19 +24,24 @@ class ConstantMatrixHermitian:
 # HELPER FUNCTIONS / DEFINITIONS:
 
 def b_t(t):
-    return 0.5*np.exp(-(t-2.5)**2)
+    return 0.5 * np.exp(-(t - 2.5) ** 2)
 
 
 def sin_t(t):
     return math.sin(t)
 
+
 sigma_x = np.matrix([
     [0 + 0j, 1 + 0j],
     [1 + 0j, 0 + 0j]])
 
-sigma_z = np.matrix([
+sigma_y = np.matrix([
     [0 + 0j, 0 - 1j],
     [0 + 1j, 0 + 0j]])
+
+sigma_z = np.matrix([
+    [1 + 0j, 0 + 0j],
+    [0 + 0j, -1 + 0j]])
 
 I_2 = np.eye(2, dtype=complex)
 
@@ -45,29 +51,10 @@ alt_ssq_system = ConstantMatrixHermitian(sigma_x, b_t, sigma_z)
 
 alt_sin_ssq_system = ConstantMatrixHermitian(sigma_x, sin_t, sigma_z)
 
-two_spin_qubit_system = ConstantMatrixHermitian(
-    np.kron(sigma_z, sigma_z),
-    b_t,
-    np.kron(sigma_x, np.eye(2, dtype=complex))
-        + np.kron(np.eye(2, dtype=complex), sigma_x))
+two_spin_qubit_system = ConstantMatrixHermitian(np.kron(sigma_z, sigma_z), b_t,
+                                                np.kron(sigma_x, I_2) + np.kron(I_2, sigma_x))
 
 single_spin_qubit = single_spin_qubit_system.at_t
 two_spin_qubits = two_spin_qubit_system.at_t
 alt_ssq = alt_ssq_system.at_t
 alt_sin_ssq = alt_sin_ssq_system.at_t
-
-# # 𝐻 = 𝐸_0 I_2 + 𝑏(𝑡)sigma_x
-# def single_spin_qubit(t, E_0=1, b=b_t):
-#     sigma_x = np.matrix([[0 + 0j, 1 + 0j], [1 + 0j, 0 + 0j]])
-#     return E_0 * np.eye(2, dtype=complex) + b(t) * sigma_x
-
-
-# def two_spin_qubits(t, D=1, b = b_t):
-#     sigma_x = np.matrix([[0 + 0j, 1 + 0j], [1 + 0j, 0 + 0j]])
-#     sigma_z = np.matrix([[0 + 0j, 0 - 1j], [0 + 1j, 0 + 0j]])
-#     constant_term = np.kron(D*sigma_z, sigma_z)
-#     varying_term = b(t)*(np.kron(sigma_x, np.eye(2, dtype=complex))+ np.kron(np.eye(2, dtype=complex), sigma_x))
-#     return constant_term + varying_term
-
-# print(single_spin_qubit(0))
-# print(two_spin_qubits(0))
